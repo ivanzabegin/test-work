f5ccce6f78071d06ac8128bbd96b330bc2aa0f61
Ivan Zabegin <IZA@opra.nl>
2022-04-16 12:21:08 +0300

    works properly

--- Program.cs
+++ Program.cs
@@ -1,9 +1,35 @@
-﻿// написать программу для, которая из имеющегосч массива строк,
+﻿// написать программу, которая из имеющегося массива строк формирует массив
 // длина которых меньше, либо равна 3 символам. Первоначальный 
 // массив строк задается с клавиатуры, либо задается вначале.
 
 Console.Clear();
 
-string [] array = {"2", "women", "get", "table"};
-Console.WriteLine(array);
+Console.Write("Количество слов");
+int n = int.Parse(Console.ReadLine());
+
+string[] wordsInArray = new string[n];
+
+Console.WriteLine($"Введите слова {n}");
+for (int i = 0; i < wordsInArray.Length; i++) wordsInArray[i] = Console.ReadLine();
+
+int count = 0; 
+
+foreach (var item in wordsInArray) 
+    if (item.Length<=3) count++;
+
+string[] finalArray = new string[count]; 
+
+count=0;
+for (int i = 0; i < wordsInArray.Length; i++) 
+    if (wordsInArray[i].Length <= 3) 
+    {
+        finalArray[count] = wordsInArray[i]; 
+        count++;
+    }
+
+Console.Write("Результат: ");
+string str = string.Join(" ", finalArray);
+Console.WriteLine($"{str}\n");
+
+
 
